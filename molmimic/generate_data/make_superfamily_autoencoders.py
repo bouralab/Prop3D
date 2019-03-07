Allow AutoEncoder trianing; fix feature calculation

molmimic/generate_data/make_superfamily_autoencoders.py
@@ -5,55 +5,116 @@ import time
 from itertools import groupby
 import glob
 
+import pandas as pd
+
 from molmimic.generate_data.iostore import IOStore
 from molmimic.generate_data.util import get_file, filter_hdf, filter_hdf_chunks
 from molmimic.generate_data.job_utils import map_job
 from molmimic.torch_model.torch_train import train
 
-def start_sae(job, sfam_id):
-    work_dir = job.fileStore.getLocalTempDir()
-    pdb_store = IOStore.get("aws:us-east-1:molmimic-full-structures")
-    feature_store = IOStore.get("aws:us-east-1:molmimic-structure-features")
+def normalize(job, features, work_dir=None):
+    """
+    one-hot: 0:18,20:23,24:26,27:30,31:34,35:37,38:70,72
+    raw: 18,19,23,26,30,34,37,71
+    normalized: 70
+    """
+    import numpy as np
+    from sklearn import preprocessing
+
+    if work_dir is None and job is not None:
+        work_dir = job.fileStore.getLocalTempDir()
+    elif work_dir is None:
+        work_dir = os.getcwd()
+
+    one_hot = ((0,18), (20,23), (24, 26), (27, 30), (31, 34), (35,37), (38,70), (72,73))
+    one_hot = [n for start, stop in one_hot for n in range(start, stop)]
+    raw = [18,19,23,26,30,34,37,71]
+    normalized = [70]
+
+    standard_scaler = preprocessing.StandardScaler()
+    minmax_scaler = preprocessing.MinMaxScaler()
+
+    for feature_f in features:
+        print(feature_f)
+        features = np.load(feature_f, mode="r")
+        print(features.shape)
+        features = features.reshape((features.shape[0], 73))
+        standard_scaler.partial_fit(features[:,raw])
+        minmax_scaler.partial_fit(features[:,raw+normalized])
+        features[:,one_hot].save(os.path.splitext(feature_f)[0]+".one_hot.npy")
+        del features
+
+    for feature_f in features:
+        base = os.path.splitext(feature_f)[0]
+        features = np.memmap(feature_f, mode="c")
+        std_scaled = standard_scaler.transform(features[:,raw])
+        features[:, raw] = std_scaled
+        np.save(base+".stdscaled.npy", features)
+        minmax_scaled = minmax_scaler.transform(features[:,raw])
+        features[:, raw] = minmax_scaled
+        np.save(base+".minmaxscaled.npy", features)
+        del features
+
+def start_sae(job, sfam_id, work_dir=None):
+    if work_dir is None and job is not None:
+        work_dir = job.fileStore.getLocalTempDir()
+    elif work_dir is None:
+        work_dir = os.getcwd()
+    # pdb_store = IOStore.get("aws:us-east-1:molmimic-full-structures")
+    # feature_store = IOStore.get("aws:us-east-1:molmimic-structure-features")
     sae_store = IOStore.get("aws:us-east-1:molmimic-structure-features")
 
-    extensions = set(["atom.npy", "residue.npy", "edges.gz"])
-    done_files = lambda k: set([f.rsplit("_", 1)[1] for f in \
-        out_store.list_input_directory(k)])
-    pdb_keys = [k for k in pdb_store.list_input_directory(str(int(sfam_id))) if \
-        k.endswith(".pdb") and extensions != done_files(os.path.splitext(k)[0])]
+    # extensions = set(["atom.npy", "residue.npy", "edges.gz"])
+    # done_files = lambda k: set([f.rsplit("_", 1)[1] for f in \
+    #     feature_store.list_input_directory(k)])
+    # pdb_keys = [k for k in pdb_store.list_input_directory(str(int(sfam_id))) if \
+    #     k.endswith(".pdb") and extensions != done_files(os.path.splitext(k)[0])]
 
-    structure_path = os.path.join(work_dir, "structures")
-    features_path = os.path.join(work_dir, "features")
+    # structure_path = os.path.join(work_dir, "structures")
+    # features_path = os.path.join(work_dir, "features")
 
     data = []
 
     sfam_id = str(int(sfam_id))
-    for feature_key in feature_store.list_input_directory(sfam_id):
-        if feature_key.endswith("atom.npy"):
-            data_key = feature_key[:-8]
+    #for feature_key in feature_store.list_input_directory(sfam_id):
+        #if feature_key.endswith("atom.npy"):
+    for feature_file in glob.glob(os.path.join(work_dir, "features", sfam_id, "*", "*_atom.npy")):
+        if True:
+            #data_key = feature_key[:-8]
+            #feature_file = feature_key #os.path.join(work_dir, "features", feature_key[len(sfam_id):])
 
-            feature_file = os.path.join(work_dir, "features", feature_key[len(sfam_id):])
-            if not os.path.isdir(os.path.dirname(feature_file)):
-                os.makedirs(os.path.dirname(feature_file))
-            feature_store.read_input_file(feature_key, feature_file)
+            # if not os.path.isdir(os.path.dirname(feature_file)):
+            #     os.makedirs(os.path.dirname(feature_file))
+            # feature_store.read_input_file(feature_key, feature_file)
 
-            pdb_key = data_key+".pdb"
-            pdb_file = os.path.join(work_dir, "structures", pdb_key[len(sfam_id):])
-            if not os.path.isdir(os.path.dirname(pdb_file)):
-                os.makedirs(os.path.dirname(pdb_file))
-            feature_store.read_input_file(pdb_key, pdb_file)
-
-            pdb, chain, sdi, domNo = os.path.basename(pdb_key).split("_")
+            # pdb_file =
+            #
+            # pdb_key = data_key+".pdb"
+            # #pdb_file = os.path.join(work_dir, "structures", pdb_key[len(sfam_id):])
+            # if not os.path.isdir(os.path.dirname(pdb_file)):
+            #     os.makedirs(os.path.dirname(pdb_file))
+            # feature_store.read_input_file(pdb_key, pdb_file)
+            print(feature_file,feature_file[:-9])
+            pdb, chain, sdi, domNo = os.path.basename(feature_file[:-9]).split("_")
             sdi, domNo = sdi[3:], domNo[1:]
 
-            data.append([data_key, pdb, chain, sdi, domNo, pdb_key, feature_key])
+            key = os.path.join(sfam_id, pdb[1:3].lower(), os.path.basename(feature_file[:-9]))
 
-    data_key = os.path.join(sfam_id, "autoencoder_keys.h5")
-    data_file = os.path.join(work_dir, "autoencoder_keys.h5" )
-    df = pd.DataFrame(data, names=["key", "pdb", "chain", "sdi", "domNo",
+            pdb_file = os.path.join(work_dir, "structures", sfam_id, pdb[1:3].lower(),
+                "{}.pdb".format(os.path.basename(feature_file[:-9])))
+
+            if not os.path.isfile(pdb_file):
+                print("Skipping "+os.path.basename(feature_file[:-8]))
+
+            data.append([key, pdb, chain, sdi, domNo, pdb_file, feature_file])
+
+    data_key = os.path.join(sfam_id, "autoencoder_keys.csv")
+    data_file = os.path.join(work_dir, "autoencoder_keys.csv" )
+    df = pd.DataFrame(data, columns=["key", "pdb", "chain", "sdi", "domNo",
         "structure", "features"])
-    df.to_hdf(data_file, "table")
-    sae_store.write_output_file(data_file, data_key)
+    df.to_csv(data_file)
+    #normalize(job, df["features"], work_dir=work_dir)
+    #sae_store.write_output_file(data_file, data_key)
     del df
     del data
 

molmimic/torch_model/torch_loader.py
@@ -7,7 +7,7 @@ from itertools import groupby
 
 try:
     import torch
-    from torch.utils.data.dataset import Dataset
+    from torch.utils.data.dataset import Dataset as _Dataset
     from torch.utils.data import DataLoader
 except ImportError:
     torch = None
@@ -17,6 +17,7 @@ except ImportError:
 import pandas as pd
 import numpy as np
 from scipy.spatial import cKDTree, distance
+from sklearn.model_selection import train_test_split
 
 try:
     import sparseconvnet as scn
@@ -24,7 +25,7 @@ except:
     #Allow module to load without scn to read data
     scn = None
 
-from molmimic.common.structure import InvalidPDB
+from molmimic.common.Structure import InvalidPDB
 from molmimic.common.voxels import ProteinVoxelizer
 from itertools import product
 
@@ -120,6 +121,32 @@ def sparse_collate(data, input_shape=(256,256,256), create_tensor=False):
         batch["weight"] = batch_weight
     return batch
 
+class Dataset(_Dataset):
+    @classmethod
+    def get_training_and_validation(cls, *args, **kwds):
+        split = kwds.get("split", 0.25)
+        if "split" in kwds:
+            del kwds["split"]
+
+        X = pd.read_csv(str(args[0]))
+
+        train, test = train_test_split(X)
+        train_args = [train]+list(args[1:])
+        kwds["train"] = True
+        test_args = [test]+list(args[1:])
+        kwds["train"] = False
+        return {"train":cls(*train_args, **kwds), "test":cls(*test_args, **kwds)}
+
+    @classmethod
+    def from_hdf(cls, *args, **kwds):
+        X = pd.read_hdf(str(args[0]), "table")
+        args = [X]+list(args[1:])
+        return cls(*args, **kwds)
+
+    def get_data_loader(self, batch_size, shuffle, num_workers):
+        return DataLoader(self, batch_size=batch_size, \
+            shuffle=shuffle, num_workers=num_workers, collate_fn=sparse_collate)
+
 class IBISDataset(Dataset):
     """IBIS Dataset used to train 3D-CNN (Unet) to predict binding site residues.
     Protein domains are split from protein structures in MMDB using their
@@ -135,36 +162,40 @@ class IBISDataset(Dataset):
     cdd : str
         Name of CDD Protein Family
     """
-    def __init__(self, data, data_directory, sfam_id=None, ppi_status="mixed", ppi_type="permanent",
-      autoencoder=False,):
+    def __init__(self, X, sfam_id=None, ppi_status="mixed", ppi_type="permanent",
+      autoencoder=False, train=True):
         assert ppi_status in ["mixed", "observed", "inferred"]
         assert ppi_type in ["all", "permanent", "weak_transient", "transient"]
-        self.data_directory = data_directory
         self.sfam_id = sfam_id
         self.ppi_status = ppi_status
         self.ppi_type = ppi_type
         self.autoencoder = autoencoder
+        self.train = train
+        self.X = X
 
         self.truth_key = "features" if autoencoder else "truth"
 
-        self.structure_path = os.path.join(data_directory, "structures")
-        self.features_path = os.path.join(data_directory, "features")
-        self.truth_path = os.path.join(data_directory, "truth")
+        self.epoch = 0
+        self.batch = 0
 
-        interfaces_path = os.path.join(get_structures_path(dataset_name), cdd, cdd)
-
-        self.X = pd.read_hdf(str(interfaces_path+".h5"), ppi_type)
+        # self.structure_path = os.path.join(data_directory, "structures")
+        # self.features_path = os.path.join(data_directory, "features")
+        # self.truth_path = os.path.join(data_directory, "truth")
 
     def __getitem__(self, index, verbose=True):
         datum = self.X.iloc[index]
 
-        pdb_file = os.path.join(self.data_directory, datum["structure"])
-        feature_file = os.path.join(self.data_directory, datum["features"])
-        truth_file = os.path.join(self.data_directory, datum[truth_key])
+        pdb_file = datum["structure"] #os.path.join(self.data_directory, datum["structure"])
+        features_file = datum["features"] #os.path.join(self.data_directory, datum["features"])
+        truth_file =  datum[self.truth_key] #os.path.join(self.data_directory, datum[truth_key])
+
+        features_path = os.path.dirname(features_file)
+        if len(os.path.basename(features_path)) == 2:
+            features_path = os.path.dirname(features_path)
 
         try:
             voxelizer = ProteinVoxelizer(pdb_file, datum.pdb, datum.chain,
-                datum.sdi, datum.domNo)
+                datum.sdi, datum.domNo, features_path=features_path)
             indices, data, truth = voxelizer.map_atoms_to_voxel_space(autoencoder=self.autoencoder)
         except (KeyboardInterrupt, SystemExit):
             raise
@@ -175,26 +206,32 @@ class IBISDataset(Dataset):
                 print(trace, file=ef)
 
             sample = {
-                "indices":      None,
-                "data":         None,
-                "truth":        None,
-                "pdb":          datum.pdb,
-                "chain":        datum.chain,
-                "sdi":          datum.sdi,
-                "domain":       datum.domNo,
-                "cdd":          self.sfam_id
+                "indices": None,
+                "data":    None,
+                "truth":   None,
+                "id":      "{}/{}/{}_{}_sdi{}_d{}".format(self.sfam_id, \
+                    datum.pdb[1:3].lower(), datum.pdb, datum.chain, datum.sdi, \
+                    datum.domNo),
+                "pdb":     datum.pdb,
+                "chain":   datum.chain,
+                "sdi":     datum.sdi,
+                "domain":  datum.domNo,
+                "sfam_id": self.sfam_id
             }
         except:
             trace = traceback.format_exc()
             print("Error:", trace)
             with open("{}_{}_{}_ibis.error".format(os.path.basename(pdb_file)[:-4], self.epoch, self.batch), "w") as ef:
-                print(trace=ef)
+                print(trace, file=ef)
             raise
 
         sample = {
             "indices":      indices,
             "data":         np.nan_to_num(data),
             "truth":        truth,
+            "id":      "{}/{}/{}_{}_sdi{}_d{}".format(self.sfam_id, \
+                datum.pdb[1:3].lower(), datum.pdb, datum.chain, datum.sdi, \
+                datum.domNo),
             "pdb":          datum.pdb,
             "chain":        datum.chain,
             "sdi":          datum.sdi,