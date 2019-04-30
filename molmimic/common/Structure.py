Docking works fore more examples: fixed biounit, etc. Fixed feature calculation. Add script to show prediction on structure

molmimic/common/Structure.py
@@ -1,6 +1,7 @@
 import os
 from io import StringIO
 
+import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 from Bio import PDB
@@ -40,15 +41,15 @@ def get_feature_names(only_aa=False, only_atom=False, use_deepsite_features=Fals
         return PDB.Polypeptide.aa3
     feature_names = [
         "C", "CT", "CA", "N", "N2", "N3", "NA", "O", "O2", "OH", "S", "SH", "Unk_atom",
-        "C", "N", "O", "S", "Unk_element",
+        "C_elem", "N_elem", "O_elem", "S_elem", "Unk_element",
         "vdw_volume", "charge", "neg_charge", "pos_charge", "neutral_charge",
-        "electrostatic_potential", "is_electropositive", "is_electronegative"
+        "electrostatic_potential", "is_electropositive", "is_electronegative",
         "cx", "is_concave", "is_convex", "is_concave_and_convex",
-        "hydrophobicity", "is_hydrophbic", "is_hydrophilic", "hydrophobicity_is_0"
+        "hydrophobicity", "is_hydrophbic", "is_hydrophilic", "hydrophobicity_is_0",
         "atom_asa", "atom_is_buried", "atom_exposed",
         "residue_asa", "residue_buried", "residue_exposed"]
-    feature_names += PDB.Polypeptide.aa3
-    feature_names += ["Unk_residue", "is_helix", "is_sheet", "Unk_SS"]
+    feature_names += PDB.Polypeptide.aa3+["Unk_residue"]
+    feature_names += ["is_helix", "is_sheet", "Unk_SS"]
     feature_names += [ #DeepSite features
         "hydrophobic_atom",
         "aromatic_atom",
@@ -57,10 +58,25 @@ def get_feature_names(only_aa=False, only_atom=False, use_deepsite_features=Fals
         "metal",
         "is_hydrogen",
     ]
-    fearues_names += [
+    feature_names += [
         "conservation_normalized",
         "conservation_scale",
         "is_conserved"
+    ]
+
+    return feature_names
+
+def get_residue_feature_names():
+    feature_names = [
+        "charge", "neg_charge", "pos_charge", "neutral_charge",
+        "electrostatic_potential", "is_electropositive", "is_electronegative",
+        "cx", "is_concave", "is_convex", "is_concave_and_convex",
+        "hydrophobicity", "is_hydrophbic", "is_hydrophilic", "hydrophobicity_is_0",
+        "residue_asa", "residue_buried", "residue_exposed"]
+    feature_names += PDB.Polypeptide.aa3
+    feature_names += [
+        "Unk_residue", "is_helix", "is_sheet", "Unk_SS",
+        "conservation_normalized", "conservation_scale", "is_conserved"
     ]
     return feature_names
 
@@ -113,10 +129,10 @@ class Structure(object):
         self.features_path = os.environ.get("MOLMIMIC_FEATURES", features_path)
 
         self.residue_features_file = os.path.join(features_path,
-            self.pdb.lower()[1:3], "{}_residue.npy".format(self.id))
+            self.pdb.lower()[1:3], "{}_residue.h5".format(self.id))
 
         self.atom_features_file = os.path.join(features_path,
-            self.pdb.lower()[1:3], "{}_atom.npy".format(self.id))
+            self.pdb.lower()[1:3], "{}_atom.h5".format(self.id))
 
         if not os.path.isdir(os.path.dirname(self.atom_features_file)):
             os.makedirs(os.path.dirname(self.atom_features_file))
@@ -131,10 +147,24 @@ class Structure(object):
         else:
             self.residue_feature_mode = feature_mode
 
-        self.residue_features = np.memmap(self.residue_features_file,
-            dtype=np.float, mode=self.residue_feature_mode, shape=(self.n_residues, self.n_residue_features))
-        self.atom_features = np.memmap(self.atom_features_file,
-            dtype=np.float, mode=self.atom_feature_mode, shape=(self.n_atoms, self.n_atom_features))
+        # self.residue_features = np.memmap(self.residue_features_file,
+        #     dtype=np.float, mode=self.residue_feature_mode, shape=(self.n_residues, self.n_residue_features))
+        # self.atom_features = np.memmap(self.atom_features_file,
+        #     dtype=np.float, mode=self.atom_feature_mode, shape=(self.n_atoms, self.n_atom_features))
+
+        if self.atom_feature_mode == "r":
+            self.atom_features = pd.read_hdf(self.atom_features_file, "table")
+        else:
+            atom_index = [a.serial_number for a in self.structure.get_atoms()]
+            self.atom_features = pd.DataFrame(columns=get_feature_names(),
+                index=atom_index)
+
+        if self.residue_feature_mode == "r":
+            self.residue_features = pd.read_hdf(self.residue_features_file, "table")
+        else:
+            residue_index = [r.get_id() for r in self.structure.get_residues()]
+            self.residue_features = pd.DataFrame(columns=get_residue_feature_names(),
+                index=residue_index)
 
     def get_atoms(self, include_hetatms=False, exclude_atoms=None):
         for a in self.filter_atoms(self.structure.get_atoms(), include_hetatms=include_hetatms, exclude_atoms=exclude_atoms):

molmimic/generate_data/BioUnit.py
@@ -11,7 +11,7 @@ from toil.realtimeLogger import RealtimeLogger
 
 parser = Bio.PDB.PDBParser(QUIET=1, PERMISSIVE=True)
 
-def check_contacts(original_complex, mol_file, mol_res, int_file, int_res):
+def check_contacts(original_complex, mol_file, mol_res, int_file, int_res, return_vars=False):
     try:
         s = parser.get_structure("ref", original_complex)
     except (SystemExit, KeyboardInterrupt):
@@ -23,7 +23,7 @@ def check_contacts(original_complex, mol_file, mol_res, int_file, int_res):
     assert len(chains)==2
 
     try:
-        ic = calculate_ic(s)
+        ic = calculate_ic(s, d_cutoff=8.)
     except ValueError:
         return False
 
@@ -34,13 +34,17 @@ def check_contacts(original_complex, mol_file, mol_res, int_file, int_res):
     face1, face2 = set([r.id[1] for r in face1]), set([r.id[1] for r in face2])
     RealtimeLogger.info("FACE1 {} {}".format(mol_res, face1))
     RealtimeLogger.info("FACE2 {} {}".format(int_res, face2))
-    full_face1 = len(face1.intersection(mol_res))/float(len(mol_res)) > 0.5
-    full_face2 = len(face2.intersection(int_res))/float(len(int_res)) > 0.5
-    return len(ic) > 0 and full_face1 and full_face2
+
+    full_face1 = len(face1.intersection(mol_res))/float(len(mol_res))
+    full_face2 = len(face2.intersection(int_res))/float(len(int_res))
+    if not return_vars:
+        return len(ic) > 0 and full_face1 >= 0.5 and full_face2 >= 0.5
+    else:
+        return ic, face1, face2, full_face1, full_face2
 
 def build_biounit(moving_mol, mol_pdb, mol_chain, moving_int, int_pdb, int_chain, work_dir):
     #Original files may be correct
-    yield moving_mol, moving_int
+    yield moving_int
 
     #Get BioUnit info for interactint domain (since inferred are Superimposed into it)
     pdbId = int_pdb
@@ -54,21 +58,22 @@ def build_biounit(moving_mol, mol_pdb, mol_chain, moving_int, int_pdb, int_chain
         raise RuntimeError("No REMARK 350")
     quat = quat350(remarks[350])
 
-    for i, (Mi, ti) in enumerate(quat):
-        if not (Mi == np.eye(3)).all() and not (ti == np.zeros(3)).all():
-            rt_mol = "{}.rottrans.{}.pdb".format(os.path.splitext(moving_mol)[0], i)
-            tidy(rottrans_from_matrix(moving_mol, Mi, ti, rt_mol), replace=True)
-        else:
-            rt_mol = moving_mol
+    # for i, (Mi, ti) in enumerate(quat):
+    #     if not (Mi == np.eye(3)).all() and not (ti == np.zeros(3)).all():
+    #         rt_mol = "{}.rottrans.{}.pdb".format(os.path.splitext(moving_mol)[0], i)
+    #         tidy(rottrans_from_matrix(moving_mol, Mi, ti, rt_mol), replace=True)
+    #     else:
+    #         rt_mol = moving_mol
 
-        for j, (Mj, tj) in enumerate(quat):
-            if not (Mj == np.eye(3)).all() and not (tj == np.zeros(3)).all():
-                rt_int = "{}.rottrans.{}.pdb".format(os.path.splitext(moving_int)[0], j)
-                tidy(rottrans_from_matrix(moving_int, Mj, tj, rt_int), replace=True)
-            else:
-                rt_int = moving_int
+    for j, (Mj, tj) in enumerate(quat):
+        #if not (Mj == np.eye(3)).all(): # and not (tj == np.zeros(3)).all():
+        rt_int = "{}.rottrans.{}.pdb".format(os.path.splitext(moving_int)[0], j)
+        tidy(rottrans_from_matrix(moving_int, Mj, tj, rt_int), replace=True)
+        # else:
+        #     continue
+        #     rt_int = moving_int
 
-            yield rt_mol, rt_int
+        yield rt_int
 
     del remarks
     del quat

molmimic/generate_data/iRMSD.py
@@ -216,7 +216,18 @@ class Complex(Select):
 
     def cns_energy(self):
         from molmimic.parsers.CNS import Minimize, calculate_energy
-        return calculate_energy(self.pdb, work_dir=self.work_dir, job=self.job)
+        try:
+            return calculate_energy(self.pdb, work_dir=self.work_dir, job=self.job)
+        except RuntimeError:
+            return pd.Series({
+                 "cns_Etotal": np.nan,
+                 "cns_grad(E)":np.nan,
+                 "cns_E(BOND)":np.nan,
+                 "cns_E(ANGL)":np.nan,
+                 "cns_E(DIHE)":np.nan,
+                 "cns_E(IMPR)":np.nan,
+                 "cns_E(VDW )":np.nan,
+                 "cns_E(ELEC)":np.nan})
 
     def haddock_score(self):
         from molmimic.parsers.haddock import score_complex