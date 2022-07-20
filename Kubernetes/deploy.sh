2f3a182a3f1a5c9e001ae3d3efa5102e311555f8
Eli Draizen <edraizen@gmail.com>
2022-07-20 18:26:20 -0400

    Add Kubernetes deploy files if ever needed

---
+++ Kubernetes/deploy.sh
@@ -0,0 +1,18 @@
+sudo kubectl delete job.batch/molimic-data-generation-toil --force --grace-period=0
+
+hsdel /home/ed4bu/cath-paper-test-0-jobstore.h5
+mc rb --force bournelab/home-ed4bu-cath-paper-test-0--files
+
+if [[ $1 == "--update-docker" ]]; then
+   rm -f molmimic-0.0.1.tar
+   sudo -E make -C ../Docker
+   sudo docker save --output molmimic-0.0.1.tar edraizen/molmimic:0.0.1
+   sudo k3s ctr images rm docker.io/edraizen/molmimic:0.0.1
+   sudo k3s ctr images import molmimic-0.0.1.tar
+fi
+
+sudo kubectl apply -f deploy.yaml
+sudo kubectl get all -l job-name==molimic-data-generation-toil
+
+echo "sudo kubectl logs -f pod/$(sudo kubectl get pods -l job-name==molimic-data-generation-toil | tail -n 1 | awk '{print $1}')"
+sudo kubectl logs -f pods/`sudo kubectl get pods -l job-name==molimic-data-generation-toil | tail -n 1 | awk '{print $1}'`
